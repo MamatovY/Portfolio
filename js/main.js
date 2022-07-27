38b0eab54e7ece4330722e251f7cc6b578934a30
MamatovY <yymanmamatov@gmail.com>
2022-07-27 23:13:46 +0300

    ф

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$(".burgerIc").click(function (e) {
-    e.preventDefault();
-    $(".headNav").toggleClass("burgerActive");
-    $("nav").toggleClass("burgerMenu");
-})
+$(document).ready(function(){
+    $(".burgerIc").click(function (e) {
+        e.preventDefault();
+        $(".headNav").slideToggle(500);
+        //*************Крестик********************** */
+        $('.bI1').toggleClass('bIactive1');
+        $('.bI3').toggleClass('bIactive2');
+        $('.bI2').toggleClass('bIactive3');
+        //********************************** */
+    })
+    })
+    
