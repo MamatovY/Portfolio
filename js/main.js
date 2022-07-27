0dd2ea947650212bcb98d79ab4f76b8d5e090403
MamatovY <yymanmamatov@gmail.com>
2022-07-27 22:46:30 +0300

    крестик

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-$(".burgerIc").click(function (e) {
-    $(".headNav").toggleClass("burgerActive");
-    $("nav").toggleClass("burgerMenu");
+$(document).ready(function(){
 
+$(".burgerIc").click(function (e) {
+    e.preventDefault();
+    $(".headNav").slideToggle(500);
+
+
+    //*************Крестик********************** */
+    $('.bI1').toggleClass('bIactive1')
+    $('.bI3').toggleClass('bIactive2')
+    $('.bI2').toggleClass('bIactive3')
+    //********************************** */
+})
 })
