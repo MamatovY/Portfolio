f49522bb07bfc3691386999ebda212ddccecb6f6
MamatovY <yymanmamatov@gmail.com>
2022-07-28 00:08:52 +0300

    ф

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $(".burgerIc").click(function (e) {
         e.preventDefault();
-        $(".headNav").slideToggle(500);
+        $(".headNav").toggleClass('burgerActive');
+        $("nav").toggleClass('burgerMenu');
+        
         //*************Крестик********************** */
         $('.bI1').toggleClass('bIactive1');
         $('.bI3').toggleClass('bIactive2');
