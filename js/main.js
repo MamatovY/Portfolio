d5d15e1b6714973bc25f7df3c77b618c2c72052b
MamatovY <yymanmamatov@gmail.com>
2022-07-30 21:27:45 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
 $(document).ready(function(){
+   
+
+    $('.topMove').click(function(e){
+        e.preventDefault();
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        });
+    })
+
+    if(pageYOffset>100){
+        $('.topMove').css('display','block');
+    }
+
+    window.addEventListener('scroll', function(){
+        console.log(pageYOffset);
+        if (pageYOffset>1400 && pageYOffset<2200 ) {
+            $(".aboutText").slideDown(1500);
+        $(".slideImg1").css("transform", "translate(10%, 0)");
+        $(".slideImg2").css("transform", "translate(0%, 0)");
+        $(".slideImg3").css("transform", "translate(-10%, 0)");
+        }
+    
+
+
+        
+        if(pageYOffset>100){
+            $('.topMove').css('display','block');
+            
+        }else{
+            $('.topMove').css('display','none');
+        }
+        
+
+    });
+
+
     $(".burgerIc").click(function (e) {
         e.preventDefault();
         $(".headNav").toggleClass('burgerActive');
         $("nav").toggleClass('burgerMenu');
+        
+
         
         //*************Крестик********************** */
         $('.bI1').toggleClass('bIactive1');
