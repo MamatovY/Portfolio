bb22b4c84cebad6005d4bf1de8f8df0c1de9d8b6
MamatovY <yymanmamatov@gmail.com>
2022-08-01 01:37:47 +0600

    s

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 
     $(window).resize(function(){
         const widthWind=$(window).width();
-        console.log(widthWind);
         if (widthWind>767) {
             if($(".headNav").hasClass("burgerActive")){
                 $(".burgerIc").trigger("click");
@@ -36,7 +35,7 @@ $(document).ready(function(){
         if (pageYOffset>1400 && pageYOffset<2200 ) {
         $(".slideImg1").css("transform", "translate(0, 0)");
         $(".slideImg2").css("transform", "translate(0, 0)");
-        $(".slideImg3").css("transform", "translate(-10px, 0)");
+        $(".slideImg3").css("transform", "translate(0, 0)");
         $(".aboutText").slideDown(1500);
         }
 
