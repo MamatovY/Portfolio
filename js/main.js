7ac4a8e44cb661ac7997acb074dfcd54e9b8b069
MamatovY <yymanmamatov@gmail.com>
2022-08-15 15:28:11 +0600

    ф

--- js/main.js
+++ js/main.js
@@ -1,61 +1,62 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
-    let anchors=document.querySelectorAll('a[href*="#"]');
-    for(anchor of anchors){
+
+    let anchors = document.querySelectorAll('a[href*="#"]');
+    for (anchor of anchors) {
         if (anchor) {
-            anchor.addEventListener("click", function(e){
+            anchor.addEventListener("click", function (e) {
                 e.preventDefault();
-                anchorId= this.getAttribute('href');
+                anchorId = this.getAttribute('href');
                 document.querySelector(anchorId).scrollIntoView({
                     behavior: 'smooth',
-                    block:'start'
+                    block: 'start'
                 })
-                
+
             })
         }
     }
-    
-    
 
-    $(window).resize(function(){
-        const widthWind=$(window).width();
-        if (widthWind>767) {
-            if($(".headNav").hasClass("burgerActive")){
+
+
+
+    $(window).resize(function () {
+        const widthWind = $(window).width();
+        if (widthWind > 767) {
+            if ($(".headNav").hasClass("burgerActive")) {
                 $(".burgerIc").trigger("click");
             }
         }
-        
+
     })
 
-    $('.topMove').click(function(e){
+    $('.topMove').click(function (e) {
         e.preventDefault();
         window.scrollTo({
-            top:0,
-            behavior:"smooth"
+            top: 0,
+            behavior: "smooth"
         });
     })
 
-    if(pageYOffset>100){
-        $('.topMove').css('display','block');
+    if (pageYOffset > 100) {
+        $('.topMove').css('display', 'block');
     }
-    
 
-    window.addEventListener('scroll', function(){
-        
-        if (pageYOffset>1400 && pageYOffset<2200 ) {
-        $(".slideImg1").css("transform", "translate(0, 0)");
-        $(".slideImg2").css("transform", "translate(0, 0)");
-        $(".slideImg3").css("transform", "translate(0, 0)");
-        $(".aboutText").slideDown(1500);
+
+    window.addEventListener('scroll', function () {
+
+        if (pageYOffset > 1400 && pageYOffset < 2200) {
+            $(".slideImg1").css("transform", "translate(0, 0)");
+            $(".slideImg2").css("transform", "translate(0, 0)");
+            $(".slideImg3").css("transform", "translate(0, 0)");
+            $(".aboutText").slideDown(1500);
         }
 
-        
-        if(pageYOffset>100){
-            $('.topMove').css('display','block');
-            
-        }else{
-            $('.topMove').css('display','none');
+
+        if (pageYOffset > 100) {
+            $('.topMove').css('display', 'block');
+
+        } else {
+            $('.topMove').css('display', 'none');
         }
     });
 
@@ -65,9 +66,9 @@ $(document).ready(function(){
         $(".headNav").toggleClass('burgerActive');
         $("nav").toggleClass('burgerMenu');
         $(".headNav").css("transform", "translate(0, 0)");
-        
 
-        
+
+
         //*************Крестик********************** */
         $('.bI1').toggleClass('bIactive1');
         $('.bI3').toggleClass('bIactive2');
@@ -77,15 +78,14 @@ $(document).ready(function(){
 
 
 
-        $(".slider").slick({
-            dots:true,
-            speed:500,
-            autoplay:true,
-            autoplaySpeed:3000,
-            pauseOnHover:true,
-            waitForAnimate:false,
-            adaptiveHeight:true
-            
-        });
-    })
-    
+    $(".slider").slick({
+        dots: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        waitForAnimate: false,
+        adaptiveHeight: true
+
+    });
+})
